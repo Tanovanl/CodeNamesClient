b4262068e4a16948adc2e53cb1a8d6f736bed1c7
tano.vanlerberghe <tano.vanlerberghe@student.howest.be>
2024-07-18 17:44:55 +0200

    Player can now join a game in the JoinGame component

--- src/JoinGame.jsx
+++ src/JoinGame.jsx
@@ -7,6 +7,8 @@ function JoinGame(){
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [filter, setFilter] = useState('');
+    const [playerName, setPlayerName] = useState('');
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -24,19 +26,26 @@ function JoinGame(){
         fetchData();
     }, []);
 
+    const joinGame = async (gameId) => {
+        const url = `/game/${gameId}/player/${playerName}`;
+        console.log(url);
+        await apiCall(url, "POST");
+    }
+
     if (loading) return <div id="join-div" className="card"> <div>Loading...</div> </div>
     if (error) return <div id="join-div" className="card">  <div>Error: {error}</div> </div>;
 
     return (
         <div id="join-div" className="card">
             <h2>Join a game</h2>
-            <input type="text" value={filter} onChange={(e) => setFilter(e.target.value)} placeholder="Filter by game name" /> {/* New input field for the filter */}
+            <input type="text" placeholder="Player name" onChange={(e) => setPlayerName(e.target.value)}/>
+            <input type="text" value={filter} onChange={(e) => setFilter(e.target.value)} placeholder="Filter by game name" />
             <div className="all-games">
-                {games.filter(game => game.gameId.includes(filter)).map((game, index) => ( // Filter the games before mapping over them
+                {games.filter(game => game.gameId.includes(filter)).map((game, index) => (
                     <div className="game" key={index}>
                         <h3>{game.gameId}</h3>
                         <p>Players: {game.players.length}</p>
-                        <button>Join</button>
+                        <button onClick={() => joinGame(game.gameId)}>Join</button>
                     </div>
                 ))}
             </div>
