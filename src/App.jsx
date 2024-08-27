55122a14a4aa9e494eefe7bcc5b5910dcd0e0cdf
Tano <tano.vanlerberghe@student.howest.be>
2024-08-27 20:39:54 +0200

    changed in aapp

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ import Card from "./Card.jsx";
 import Board from "./Board.jsx";
 import Host from "./Host.jsx";
 import TeamJoin from "./TeamJoin.jsx";
+import JoinGame from "./JoinGame.jsx";
 function App() {
 
   return (
     <>
         <div className="container">
+            <Host/>
+            <JoinGame/>
             <TeamJoin team="RED"/>
             <Board/>
 
